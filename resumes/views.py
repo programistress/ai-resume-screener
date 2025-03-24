5e2d34e7601c535bd1f9c7e68a4d17946234eeb0
programistress <programistress@gmail.com>
2025-03-24 13:40:13 -0400

    Set up Django project and added resume upload feature

---
+++ resumes/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .forms import ResumeUploadForm
+
+def upload_resume(request):
+    if request.method == 'POST':
+        form = ResumeUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ResumeUploadForm()
+    return render(request, 'upload.html', {'form': form})
